a64c4ad6a43fde3e214a4dc2749ffd3f37915175
martin.luchev <martin.luchev@mentormate.com>
2017-01-03 18:36:20 +0200

    start + added navbar

---
+++ final-project/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+import {NavbarComponent} from './navbar/navbar.component';
+
+@Component({
+    selector: 'my-app',
+    template: '<navbar></navbar>',
+    directives: [NavbarComponent]
+})
+export class AppComponent { }
---
+++ final-project/app/navbar/navbar.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'navbar',
+    templateUrl: 'app/navbar/navbar.component.html'
+})
+export class NavbarComponent { }
